f2a01c4cb07e1133cdd365a5b3d843dfeb8d6d2b
a0i-it <github@example.com>
2023-10-27 22:52:35 +0900

    削除ボタンの追加と表示切り替えができた

--- app/src/main/java/com/example/checklist/MainActivity.kt
+++ app/src/main/java/com/example/checklist/MainActivity.kt
@@ -2,36 +2,80 @@ package com.example.checklist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.SpannableStringBuilder
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
-    //７）recyclerViewの変数を用意
-    private var parentCheckList =ArrayList<CheckListData>()
-    private lateinit var recyclerView:RecyclerView
-    private val recyclerAdapter = RecyclerAdapter(parentCheckList)
+    // recyclerViewの変数を用意
+    private lateinit var parentCheckList: ArrayList<CheckListData>
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var recyclerAdapter: RecyclerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //８）recyclerView表示の呪文
-        recyclerView =findViewById(R.id.rv) //idの取得
+        // recyclerView表示の呪文
+        recyclerView = findViewById(R.id.rv) //idの取得
+        parentCheckList = ArrayList()
+        val btnEdit : Button = findViewById(R.id.editBtn)
+        var editPosition : Int = -1
+
+        // recyclerAdapter を初期化
+        recyclerAdapter = RecyclerAdapter(parentCheckList, object : RecyclerAdapter.OnItemClickListener {
+            override fun onItemClick(view: View, position: Int, clickedText: String) {
+                editPosition = position
+                val btnEditText = btnEdit.text
+                val editText : EditText = findViewById(R.id.editText)
+                if (btnEditText == "完了"){
+                    editText.text = SpannableStringBuilder(clickedText)
+                    // カーソルを文字列の最後尾に移動
+                    editText.setSelection(editText.text.length)
+                }
+            }
+        })
+
         recyclerView.adapter = recyclerAdapter //アダプターをセット
         recyclerView.layoutManager = LinearLayoutManager(this) //各アイテムを縦に並べてください（見せ方の指示）
 
         val btnAdd : Button = findViewById(R.id.addBtn)
+        val et : EditText = findViewById(R.id.editText)
         btnAdd.setOnClickListener {
-            val et : EditText = findViewById(R.id.editText)
             val inputText = et.text.toString()
             if (inputText.isBlank()) return@setOnClickListener
-            val data =CheckListData(inputText)
-            parentCheckList.add(data)
-            recyclerAdapter.notifyItemInserted(parentCheckList.lastIndex) //表示を更新（リストの最後に挿入）
+            if (btnEdit.text == "完了" && editPosition != -1) {
+                // 更新モードの場合、クリックされたアイテムを新しいテキストで更新
+                val updatedData = CheckListData(inputText)
+                parentCheckList[editPosition] = updatedData
+                recyclerAdapter.notifyItemChanged(editPosition)
+                editPosition = -1
+            } else {
+                // 追加モードの場合、新しいデータをリストに追加
+                val data = CheckListData(inputText)
+                parentCheckList.add(data)
+                recyclerAdapter.notifyItemInserted(parentCheckList.lastIndex)
+            }
 
             et.text = null
+        }
+
+        btnEdit.setOnClickListener {
+            val btnEditText = btnEdit.text
+            if (btnEditText == "編集"){
+                btnEdit.text = "完了"
+                btnAdd.text = "更新"
+                recyclerAdapter.deleteIconVisible = true // deleteIconを表示
+            } else {
+                btnEdit.text = "編集"
+                btnAdd.text = "追加"
+                et.text = null
+                recyclerAdapter.deleteIconVisible = false // deleteIconを非表示
+            }
         }
     }
 }
--- app/src/main/java/com/example/checklist/RecyclerAdapter.kt
+++ app/src/main/java/com/example/checklist/RecyclerAdapter.kt
@@ -3,24 +3,68 @@ package com.example.checklist
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
-class RecyclerAdapter(private val checkListData: ArrayList<CheckListData>) : RecyclerView.Adapter<RecyclerAdapter.ViewHolderItem>() {
+class RecyclerAdapter(private val checkListData: ArrayList<CheckListData>, private val itemClickListener: OnItemClickListener) : RecyclerView.Adapter<RecyclerAdapter.ViewHolderItem>() {
+
+    var deleteIconVisible: Boolean = false
+        set(value) {
+            field = value
+            notifyDataSetChanged() // フラグが変更されたらリスト全体を更新
+        }
 
     //５）ViewHolder（インナークラス）
-    inner class ViewHolderItem(v: View) :RecyclerView.ViewHolder(v) {
+    inner class ViewHolderItem(v:View,rAdapter:RecyclerAdapter) :RecyclerView.ViewHolder(v) {
         val tvHolder : TextView = v.findViewById(R.id.tv)
+        val deleteIcon: ImageView = v.findViewById(R.id.deleteIcon)
+
+        fun bind(data: CheckListData) {
+            // 他のバインド処理
+            if (deleteIconVisible) {
+                deleteIcon.visibility = View.VISIBLE
+            } else {
+                deleteIcon.visibility = View.GONE
+            }
+        }
+
+        // クリック処理(1行分の画面(view)が押されたら～)
+        init {
+            v.setOnClickListener {
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    itemClickListener.onItemClick(itemView, position, checkListData[position].checkList)
+                }
+            }
+        }
+    }
+
+    interface OnItemClickListener {
+        fun onItemClick(view: View, position: Int, clickedText: String)
+    }
+
+    private var selectedPosition: Int = RecyclerView.NO_POSITION
+
+    fun getSelectedPosition(): Int {
+        return selectedPosition
+    }
+
+    fun setSelectedPosition(position: Int) {
+        selectedPosition = position
+        notifyDataSetChanged()
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderItem {
         val itemXml = LayoutInflater.from(parent.context)
             .inflate(R.layout.one_layout, parent, false)
-        return ViewHolderItem(itemXml)
+        return ViewHolderItem(itemXml, this)
     }
 
     override fun onBindViewHolder(holder: ViewHolderItem, position: Int) {
         val currentItem = checkListData[position] //何番目のリスト（アイテム）ですか
+        holder.bind(currentItem) // bindメソッドを呼び出してアイテムをバインド
         holder.tvHolder.text = currentItem.checkList //そのリストの中の要素を指定して代入
     }
 
